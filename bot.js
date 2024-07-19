ed758cd23c34b4f898262bbc5f19d55419235a50
Miguel <122368297+27mfp@users.noreply.github.com>
2024-07-19 10:53:54 +0100

    fix: removed web server

--- bot.js
+++ bot.js
@@ -1,17 +1,5 @@
 require("dotenv").config();
 
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
-
 const {
   Client,
   GatewayIntentBits,
